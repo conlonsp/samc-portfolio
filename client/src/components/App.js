c668810abd07e8849ab924b2ed1130ed2bf386e0
samuelconlon <conlonsp@yahoo.com>
2023-11-23 10:25:49 -0400

    write first route for NavBar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
+import Home from '../pages/Home';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <Routes>
+        <Route path='/' element={
+          <Home />
+        }/>
+      </Routes>
     </div>
   );
 }
