4e4500b7d850a285c284b7e502f33ae399815606
samuelconlon <conlonsp@yahoo.com>
2023-11-23 10:40:45 -0400

    add Portfolio route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './Header';
 import Home from '../pages/Home';
 import About from '../pages/About';
+import Portfolio from '../pages/Portfolio';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         }/>
         <Route path='/about' element={
           <About />
+        }/>
+        <Route path='/portfolio' element={
+          <Portfolio />
         }/>
       </Routes>
     </div>
