e02d8ee345aa2fc76f87c0a374d7709d8525a9b7
samuelconlon <conlonsp@yahoo.com>
2023-11-23 10:51:51 -0400

    add not_found error handling

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :projs_not_found
+  
   def index
     render json: Project.all, status: :ok
   end
@@ -6,5 +8,11 @@ class ProjectsController < ApplicationController
   def show
     proj = Project.find(params[:id])
     render json: proj, status: :ok
+  end
+
+  private
+
+  def projs_not_found
+    render json: { errors: ['Project not found'] }, status: :not_found
   end
 end
