f2c04735f28a5d1378dddffb6bfdef6deb41cdf9
samuelconlon <conlonsp@yahoo.com>
2023-11-23 09:48:20 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+projects = Project.create([
+  {
+    name: 'BrewTap',
+    tech: 'JavaScript, CSS',
+    func: 'This application utilizes an API from openbrewerydb.org.  BrewTap allows the user to look up all breweries in their area by typing in their city and state into the search bars.',
+    yt_id: 'ov0QVmrD_EU',
+    gh_url: 'https://github.com/conlonsp/phase-1-project'
+  },
+  {
+    name: 'Skate It.',
+    tech: 'JavaScript, React, CSS',
+    func: 'Skate it was created to allow skateboarders to upload or find DIY skateparks in cities and towns accross the US!',
+    yt_id: '7l4vsys4628',
+    gh_url: 'https://github.com/conlonsp/skate-it-app'
+  },
+  {
+    name: 'Homies',
+    tech: 'JavaScript, React, Ruby, SemanticUI',
+    func: 'Homies is a Zillow-like app for clients to view, post, update, and delete homes, as well as, view, create, and delete appointments and real estate agents.',
+    yt_id: '8kRZ3mhf-HQ',
+    gh_url: 'https://github.com/conlonsp/my-app-frontend, https://github.com/conlonsp/my-app-backend'
+  },
+  {
+    name: 'Brainstorm',
+    tech: 'JavaScript, React, Ruby on Rails, MaterialUI',
+    func: 'BrainStorm was created to provide a platform for entrepreneurs, innovators, and inventors to share and collaborate on ideas, gain support, and make lasting connections by engaging in what they love doing! ',
+    yt_id: 'WToScm9Oo_k',
+    gh_url: 'https://github.com/conlonsp/brainstorm-app'
+  },
+  {
+    name: 'Wild Tracker',
+    tech: 'JavaScript, React, Ruby on Rails, D3.js, CSS',
+    func: 'The Wild Tracker serves as a platform for users to engage with wildlife and conservation organizations, explore ongoing projects, make donations, and track their impact. It promotes awareness, involvement, and support towards wildlife conservation efforts while providing users with a personalized experience and the ability to contribute to specific projects and organizations they are passionate about.',
+    yt_id: 'TmDbQJ65ob0',
+    gh_url: 'https://github.com/conlonsp/wild-tracker-app'
+  }
+])
+
