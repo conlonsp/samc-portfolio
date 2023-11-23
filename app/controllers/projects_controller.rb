00f4e64f08e03f0c71b5c3903bda6590154660a5
samuelconlon <conlonsp@yahoo.com>
2023-11-23 09:44:03 -0400

    create show action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,9 @@ class ProjectsController < ApplicationController
   def index
     render json: Project.all, status: :ok
   end
+
+  def show
+    proj = Project.find(params[:id])
+    render json: proj, status: :ok
+  end
 end
