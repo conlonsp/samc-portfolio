e157b93516ec3e4e0d7d37f80e7ca283378f6074
samuelconlon <conlonsp@yahoo.com>
2023-12-01 14:09:24 -0500

    import headshot in About

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import headshot from '../images/headshot-copy.jpg'
 
 function About() {
 
   return (
     <div>
       <h1>About</h1>
+      <img src={headshot} alt='headshot'/>
     </div>
   )
 }
