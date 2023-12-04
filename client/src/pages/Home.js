31d7667fb4f54c67c5bb59db1c5b7266631f80b9
samuelconlon <conlonsp@yahoo.com>
2023-12-04 14:28:39 -0500

    add text to Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
 
   return (
-    <h1>Home</h1>
+    <div>
+      <h1>Home</h1>
+      <div>
+        <h4>Introduction/Greeting goes here</h4>
+        <p>
+          This is my introduction. This is my greeting. This is my introduction. This is my greeting. This is my introduction. This is my greeting. This is my introduction. This is my greeting.
+        </p>
+      </div>
+      <div>
+        <h4>Professional Tagline/Mission Statement</h4>
+        <p>
+          This is my tagline. This is my mission statement. This is my tagline. This is my mission statement.
+        </p>
+      </div>
+      <div>
+        <h4>Social Media Links:</h4>
+        <span>
+          <Link to='https://www.linkedin.com/in/sam-conlon/'>LinkedIn</Link>
+          <Link to='https://github.com/conlonsp'>Github</Link>
+          <Link to='https://medium.com/@conlonsp'>Blog</Link>
+        </span>
+      </div>
+      <div>
+        <h4>Contact Info:</h4>
+        <p>seconlonsp@gmail.com</p>
+      </div>
+    </div>
   )
 }
 
